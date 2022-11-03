72553e9fb2b202c82ff7a07e7cc65b205273014f
garrett9913 <gcoley@deloitte.com>
2022-11-03 17:04:15 +0000

    changed team name

--- src/app.config.js
+++ src/app.config.js
@@ -4,7 +4,7 @@
  * DO NOT store state here - must be part of a stateless service
  */
 class AppConfig {
-    team = 'Your Team Name';
+    team = 'team G';
     version = '1.0.0';
 }
 module.exports = AppConfig;
